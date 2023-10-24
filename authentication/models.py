4e38821c6d526b5f5a64a6539a3dfa5037f2df96
asteriskzie <ester.gracia.10@gmail.com>
2023-10-24 22:16:48 +0700

    Add comment to authentication models

--- authentication/models.py
+++ authentication/models.py
@@ -3,14 +3,16 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Subject(models.Model) : 
-    name = models.CharField(max_length=255)
+    # model subject / genre sebuah buku 
+    name = models.CharField(max_length=255)     # nama subject tersebut, misalnya 'science fiction'
 
     def __str__(self): 
         return self.name
 
 class Member(models.Model) : 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    subjects = models.ManyToManyField(Subject, related_name="members")
+    # model 'user' BookMate, dinamai Member supaya tidak ambigu dengan model User bawaan Django 
+    user = models.OneToOneField(User, on_delete=models.CASCADE)         # username dan email menggunakan model User bawaan Django
+    subjects = models.ManyToManyField(Subject, related_name="members")  # subject-subject yang member sukai
 
     def __str__(self): 
         return self.user.username 
