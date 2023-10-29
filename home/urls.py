906093b8c2472dd1812964c815955dc6be39ceb6
asteriskzie <ester.gracia.10@gmail.com>
2023-10-29 18:09:30 +0700

    Fix integration with review module

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,4 @@ app_name = 'home'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
-    path('test/', test, name='test'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from books.models import Book
+from dashboardbuku.models import Review
+from authentication.models import Member
 import json 
 from django.http import HttpResponse
 
 def which_home(request): 
-    return 1
+
+    # kalau profil belum lengkap, 1 
+    # kalau belum ada review, 2 
+    member = Member.objects.get(account=request.user)
+    have_reviewed = Review.objects.filter(reviewer=member).count() > 0
+    if (not have_reviewed): 
+        return 2
+    # sisanya 3 
+    return 3
 
 # Create your views here.
 def show_main(request): 
@@ -15,7 +25,3 @@ def show_main(request):
         }
         return render(request, 'home.html', context=context) 
     return render(request, 'landing.html')
-
-
-def test(request): 
-    return render(request, 'test.html')
