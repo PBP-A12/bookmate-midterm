cf610ebdc7c843c45910fd06e4ddfc637221d4d5
vinkakniv <vinka.aeris@gmail.com>
2023-12-10 16:35:56 +0700

    coba dulu huwa

--- match/urls.py
+++ match/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path 
 from match.views import *
-from .views import show_match, get_match, accept_recommendation, redirect
+from .views import show_match, get_match, accept_recommendation, redirect, get_match_flutter, accept_flutter
 
 app_name = 'match'
 
@@ -8,5 +8,7 @@ urlpatterns = [
     path('', show_match, name='show_match'),
     path('get-match/', get_match, name='get_match'),
     path('accept/<int:id>/', accept_recommendation, name='accept'),
-    path("user/<int:id>/", redirect, name="redirect")
+    path("user/<int:id>/", redirect, name="redirect"),
+    path('get_match_flutter/', get_match_flutter, name='get_match_flutter'),
+    path('accept-flutter/<int:id>/', accept_flutter, name='accept_flutter'),
 ]
--- match/views.py
+++ match/views.py
@@ -1,11 +1,12 @@
+import logging
 from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpRequest, HttpResponse, HttpResponseRedirect, HttpResponseNotFound, JsonResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect, HttpResponseNotFound, HttpResponseServerError, JsonResponse
 from django.urls import reverse
 from django.core import serializers
 from book_request.serializers import BookRequestSerializer
-from match.serializers import MatchingSerializer
+from django.core.serializers import serialize
 from match.models import Matching
 from user.models import Profile
 from book_request.models import BookRequest
@@ -47,7 +48,7 @@ def get_match(request):
         new_match = Matching(user = this_member, matched_member = other_member)
     new_match.save()
     
-    interest = "-"
+    interest = get_interest(other_member)
     result = {
         "name" : other_member.account.username, 
         "first_name": other_member.account.first_name,
@@ -108,63 +109,99 @@ def match_interest(this_member, other_member):
     else:
         interest_subject = random.choice(list(intersection_result))
     return interest_subject
+    
 
+def get_interest(other_member):
+    other_interest_book = BookRequest.objects.filter(member=other_member).first()
+
+    if not other_interest_book:
+        return ""
+
+    # Extract names directly from the ManyToManyField
+    interest_names = other_interest_book.subjects.values_list('name', flat=True)
+    
+    # Take only the first 3 subjects
+    interest_names = list(interest_names)[:3]
+
+    # Join the names into a single string separated by commas
+    interest_string = ', '.join(interest_names)
+
+    return interest_string
+
+def get_matching_flutter(request):
+    this_member = Member.objects.get(account = request.user)
 
 def get_match_json(request):
     data = serializers.serialize('json', Matching.objects.all())
     return HttpResponse(data, content_type='application/json')
 
 
-@login_required
-@csrf_exempt
-def get_match_flutter(request):
-    this_member = Member.objects.get(account = request.user)
-    other_member = None 
-    for user_i in Member.objects.order_by("?") : 
-        is_matched = Matching.objects.filter(user = this_member, matched_member = user_i, accepted = True).exists()
-        if (user_i != this_member and not is_matched and not user_i.account.is_superuser) :
-            other_member = user_i 
-            break 
-    
-    if (other_member is None) : 
-        return JsonResponse({"status": 'failed', "message": "no member!"}, status=400)
-  
-    other_profile = Profile.objects.get(member = other_member)
-    new_match = Matching.objects.filter(user = this_member, matched_member = other_member, accepted = False)
-    if (new_match) :
-        new_match = new_match.first() 
-    else : 
-        new_match = Matching(user = this_member, matched_member = other_member)
-    new_match.save()
-    
-    interest = "-"
-    result = {
-        "name" : other_member.account.username, 
-        "first_name": other_member.account.first_name,
-        "last_name": other_member.account.last_name,
-        "id" : other_member.account.pk, 
-        "matching_id" : new_match.pk,
-        "interest_subject" : interest, # match_interest(this_member, other_member)
-        "bio" :  other_profile.bio,
-    }
-    return HttpResponse(json.dumps(result), content_type="application/json")    # pass
+
 
 @csrf_exempt
-def accept_recommendation_flutter(request):
+def accept_flutter(request):
     data = json.loads(request.body)
-    name = data[""]
-    first_name = data[""]
-    recommendation = Matching.objects.get()
-    print(recommendation.matched_member)
+    name = data['name']
+    interest = data['interest']
+    bio = data['bio']
+    matching_id = data['matching_id']
+    recommendation = Matching.objects.get(pk=matching_id,matched_member=Member.objects(account=name))
+
     if request.user == recommendation.user.account:
         recommendation.accepted = True
         recommendation.save()
+
         if is_receiver(recommendation.user, recommendation.matched_member):
             recommendation.user.match_received.add(recommendation.matched_member)
             recommendation.matched_member.match_received.add(recommendation.user.match_received)
+
         if not is_receiver(recommendation.user, recommendation.matched_member):
             recommendation.user.match_sent.add(recommendation.matched_member)
-        return JsonResponse({ "status": 'success', "message": "Request created successfully!"}, status=200)
+
+        return HttpResponse("Recommendation Accepted", status=200)
+    else:
+        return HttpResponse("Unauthorized", status=401)
+
+# @login_required
+@csrf_exempt   # disini
+def get_match_flutter(request):
+    this_member = Member.objects.get(account=request.user)
+    other_member = None
+
+    for user_i in Member.objects.order_by("?"):
+        is_matched = Matching.objects.filter(user=this_member, matched_member=user_i, accepted=True).exists()
+        if (user_i != this_member and not is_matched and not user_i.account.is_superuser):
+            other_member = user_i
+            break
+
+    if other_member is None:
+        return HttpResponseNotFound("No Member", status=404)
+
+    other_profile = Profile.objects.get(member=other_member)
+    new_match = Matching.objects.filter(user=this_member, matched_member=other_member, accepted=False)
+
+    if new_match:
+        new_match = new_match.first()
+    else:
+        new_match = Matching(user=this_member, matched_member=other_member)
+        new_match.save()
+
+    interest = get_interest(other_member)
+    result = {
+        "name": other_member.account.username,
+        "first_name": other_member.account.first_name,
+        "last_name": other_member.account.last_name,
+        "id": other_member.account.pk,
+        "matching_id": new_match.pk,
+        "interest_subject": interest,
+        "bio": other_profile.bio,
+    }
+    
+    logging.info('get_match endpoint reached successfully')
+
+    return JsonResponse(result)
+
+
 
 
 from user.views import user
